9a4b4f5867ad973a7a2172370233091f31d7576b
Jordan Saethre <jordan.saethre@gmail.com>
2020-11-22 17:10:42 -0700

    added second fully connected layer

--- code/models/vgg.py
+++ code/models/vgg.py
@@ -37,12 +37,14 @@ class VGG16_model(nn.Module):
         self.fc1 = nn.Linear(1000, hidden_size)
         self.bn = nn.BatchNorm2d(hidden_size)
         self.dropout = nn.Dropout(dropout)
+        self.fc2 = nn.Linear(hidden_size, num_classes)
         
     def forward(self,x):
         x = self.model(x)
         x = self.flatten(x)
         x = self.fc1(x)
+        #x = self.bn(x) TODO figure out how batch norm is used here
         x = F.relu(x)
         x = self.dropout(x)
-        #x = self.bn(x) TODO figure out how batch norm is used here
+        x = self.fc2(x)
         return x
